f0eb77598ef590c1788fafc4b794cb971647c421
fernando.martins <fernandomartins996@gmail.com>
2020-06-14 14:31:44 +0100

    Removed unused code.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,6 @@
 
     <div class="pr-16">
       <!-- Navigation links -->
-
       <template v-for="link in menu">
         <v-btn :to="link.path" text v-bind:key="link.text" :color="appBarIconColor">{{ link.text }}</v-btn>
       </template>
@@ -12,7 +11,7 @@
       <!-- Social networks -->
       <v-tooltip bottom v-for="(social, key) in socialNetworks" v-bind:key="key">
         <template v-slot:activator="{ on, attrs }">
-          <v-btn v-bind="attrs" v-on="on" :href="social.href" target="_blank" icon color="black">
+          <v-btn v-bind="attrs" v-on="on" :href="social.href" target="_blank" icon color="white">
             <v-icon>{{ social.icon }}</v-icon>
           </v-btn>
         </template>
@@ -30,21 +29,7 @@ export default {
   data: () => ({
     offsetTop: 0,
     socialNetworks: socialNetworks,
-    menu: menu,
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
+    menu: menu
   }),
   computed: {
     // a computed getter
